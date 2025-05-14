4c9f7eeea1b400283809cc51b0e298e028f55610
Amiel Brencis Salipande <absalipande@gmail.com>
2025-05-14 19:09:19 +0800

    feat: Created the CRUD features for Article

---
+++ views/article/view.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/** @var yii\web\View $this */
+/** @var app\models\Article $model */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Articles', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="article-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'title',
+            'slug',
+            'body:ntext',
+            [
+                'attribute' => 'created_at',
+                'format' => ['datetime', 'php:Y-m-d H:i:s'],
+            ],
+            [
+                'attribute' => 'updated_at',
+                'format' => ['datetime', 'php:Y-m-d H:i:s'],
+            ],
+        ],
+    ]) ?>
+
+
+</div>
--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set to false for production
-defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require __DIR__ . '/../vendor/autoload.php';
