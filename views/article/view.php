b14678117572c7b8c9f78ca5fcc6b9d7c6ff58e2
Amiel Brencis Salipande <absalipande@gmail.com>
2025-05-16 15:34:22 +0800

    fix: UI changes to the view component

--- views/article/view.php
+++ views/article/view.php
@@ -3,60 +3,40 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
-/** @var yii\web\View $this */
-/** @var app\models\Article $model */
+/* @var $this yii\web\View */
+/* @var $model app\models\Article */
 
 $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Articles', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
 ?>
-
 <div class="article-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <?php if (!Yii::$app->user->isGuest && Yii::$app->user->id === $model->created_by): ?>
+        <p>
+            <?php echo Html::a('Update', ['update', 'slug' => $model->slug], ['class' => 'btn btn-primary']) ?>
+            <?php echo Html::a('Delete', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
+    <?php endif ?>
 
-    <p class="text-muted">
-        <small>
-            Created at: <?= Yii::$app->formatter->asRelativeTime($model->created_at) ?>
-            By: <?= $model->createdBy ? Html::encode($model->createdBy->username) : '(not set)' ?>
-        </small>
-    </p>
-
-    <div class="mb-3">
-        <?= Html::encode($model->body) ?>
+    <div>
+        <h1><?php echo Html::encode($model->title) ?></h1>
+        <p class="text-muted">
+            <small>
+                Created at: <?php echo Yii::$app->formatter->asRelativeTime($model->created_at) ?>
+                By: <?php echo $model->createdBy->username ?>
+            </small>
+        </p>
+        <div>
+            <?php echo Html::encode($model->body); ?>
+        </div>
     </div>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'slug',
-            'body:ntext',
-            [
-                'attribute' => 'created_at',
-                'format' => ['datetime', 'php:Y-m-d H:i:s'],
-            ],
-            [
-                'attribute' => 'updated_at',
-                'format' => ['datetime', 'php:Y-m-d H:i:s'],
-            ],
-            [
-                'label' => 'Created By',
-                'value' => $model->createdBy ? $model->createdBy->username : '(not set)',
-            ]
-        ],
-    ]) ?>
 
 </div>
